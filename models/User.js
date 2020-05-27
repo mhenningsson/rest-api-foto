5448718adc080b153303a017fc26a10217776499
Malin Henningsson <malinhenningsson@live.se>
2020-05-27 13:58:34 +0200

    Added login method to User model.

--- models/User.js
+++ models/User.js
@@ -6,11 +6,29 @@ const bcrypt = require('bcrypt');
 module.exports = (bookshelf) => {
 	return bookshelf.model('User', {
 		tableName: 'users',
-		albums() {
+		album() {
 			return this.belongsToMany('Album');
 		},
-		photos() {
-			return this.belongsToMany('Photos');
+		photo() {
+			return this.belongsToMany('Photo');
+		}
+	}, {
+		hashSaltRounds: 10,
+
+		login: async function(email, password) {
+			// check if user exists
+			const user = await new this({email}).fetch({require: false});
+			if (!user) {
+				return false;
+				};
+
+			// get hashed password from db
+			const hash = user.get('password');
+
+			// compare new hash with db, return if match
+			return (await bcrypt.compare(password, hash))
+			? user
+			: false;
 		}
 	});
 };
